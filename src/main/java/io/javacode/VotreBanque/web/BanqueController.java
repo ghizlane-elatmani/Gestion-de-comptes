d42f47d6e618909f3c151b887f53908276949bf9
riri <ghizlane.elatmani.99@gmail.com>
2021-09-15 09:25:18 +0100

    1.8.0 - comptes.html: add section for operation

--- src/main/java/io/javacode/VotreBanque/web/BanqueController.java
+++ src/main/java/io/javacode/VotreBanque/web/BanqueController.java
@@ -1,8 +1,10 @@
 package io.javacode.VotreBanque.web;
 
 import io.javacode.VotreBanque.entities.Compte;
+import io.javacode.VotreBanque.entities.Operation;
 import io.javacode.VotreBanque.service.IBanqueMetier;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,8 +22,11 @@ public class BanqueController {
 
     @RequestMapping("/consulterCompte")
     public String consulter(Model model, String codeCompte){
+        model.addAttribute("codeCompte", codeCompte);
         try {
             Compte cp = banqueMetier.consulterCompte(codeCompte);
+            Page<Operation> operationPage = banqueMetier.listOperation(codeCompte, 0, 4);
+            model.addAttribute("listOperations", operationPage.getContent());
             model.addAttribute("compte", cp);
         } catch (Exception e){
             model.addAttribute("exception", e);
