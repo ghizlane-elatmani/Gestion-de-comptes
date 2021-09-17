ffcdf917ca9c094e3db0ab3ed74d4f12a64c0ee4
riri <ghizlane.elatmani.99@gmail.com>
2021-09-17 11:16:17 +0100

    1.13.0 - Using H2 database for SpringSecurity

--- src/main/java/io/javacode/VotreBanque/security/SecurityConfig.java
+++ src/main/java/io/javacode/VotreBanque/security/SecurityConfig.java
@@ -1,17 +1,30 @@
 package io.javacode.VotreBanque.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
+import javax.sql.DataSource;
+
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    private DataSource dataSource;
+
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username as principal, password as credentials, active from user where username=?")
+                .authoritiesByUsernameQuery("select username as principal, role as role from users_roles where username=?")
+                .rolePrefix("ROLE_");
+
+        /*
         auth.inMemoryAuthentication()
                 .withUser("admin")
                 .password("{noop}1234")
@@ -21,10 +34,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .withUser("user")
                 .password("{noop}1234")
                 .roles("USER");
+
+        */
     }
+
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers("/").permitAll()
+                .antMatchers("/h2-console/**").permitAll();
+
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
+
         http.formLogin().loginPage("/login");
         http.authorizeRequests()
                 .antMatchers("/operations", "/consulerCompte")
